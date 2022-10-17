9464ab1795d0e01af879466cac86ff4061471222
makbarsyahwana <makbarsyahwana@gmail.com>
2022-10-17 13:37:51 +0700

    add product data json

---
+++ utils/product-data.ts
@@ -0,0 +1,34 @@
+const productData = [
+    {
+        image: "/product-competitor@2x.png",
+        name: "Susu Kental Manis",
+        price: "Rp 10,000,00",
+        sold: "100"
+    },
+    {
+        image: "/product-competitor@2x.png",
+        name: "Susu Kental Manis",
+        price: "Rp 10,000,00",
+        sold: "100"
+    },
+    {
+        image: "/product-competitor@2x.png",
+        name: "Susu Kental Manis",
+        price: "Rp 10,000,00",
+        sold: "100"
+    },
+    {
+        image: "/product-competitor@2x.png",
+        name: "Susu Kental Manis",
+        price: "Rp 10,000,00",
+        sold: "100"
+    },
+    {
+        image: "/product-competitor@2x.png",
+        name: "Susu Kental Manis",
+        price: "Rp 10,000,00",
+        sold: "100"
+    },
+]
+
+export default productData
